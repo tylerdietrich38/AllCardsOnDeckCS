93ea1fb1029066c5b6a904b52f1bfbed79374bbe
Tyler Dietrich <tylerdietrich38@gmail.com>
2021-04-14 19:12:11 -0400

    All Cards On Deck

--- Program.cs
+++ Program.cs
@@ -1,12 +1,89 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace AllCardsOnDeckCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Card Shuffling");
+
+            Console.WriteLine(" ");
+
+            var cards = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+            var suits = new List<string>() { "Hearts", "Diamonds", "Spades", "Clubs" };
+
+            var deck = new List<string>();
+
+
+            for (var index = 0; index < suits.Count; index++)
+            {
+                for (var numbersIndex = 0; numbersIndex < cards.Count; numbersIndex++)
+                {
+                    var fullNumber = ($"{cards[numbersIndex]} of {suits[index]} ");
+                    deck.Add(fullNumber);
+                }
+            }
+            foreach (var num in deck)
+            {
+                Console.WriteLine($"{num}");
+            }
+
+            Console.WriteLine("------------------------------------------------");
+
+            var numberOfCards = deck.Count;
+
+            // for (var rightIndex = numberOfNumbers - 1; rightIndex >= 0; rightIndex--)
+
+            for (var rightIndex = numberOfCards - 1; rightIndex >= 0; rightIndex--)
+            {
+                var leftIndex = new Random().Next(0, rightIndex);
+
+                var rightCard = deck[rightIndex];
+
+                deck[rightIndex] = deck[leftIndex];
+
+                deck[leftIndex] = rightCard;
+            }
+
+            foreach (var num in deck)
+            {
+                Console.WriteLine($"{num}");
+            }
+
+
+
+
+            // var deck = new List<string>() { "Hearts", "Diamonds", "Spades", "clubs" };
+
+            // var numberOfSuitsInList = suits.Count;
+
+            // var cards = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+            // for (var index = 0; index < suits.Count; index++)
+            // {
+            //     var suit = suits[index];
+            //     Console.WriteLine(suit);
+            // }
+
+            // var cards = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+            // for (var index = 0; index < cards.Count; index++)
+            // {
+            //     var card = cards[index];
+            //     Console.WriteLine(card);
+            // }
+
+
+            // var randomNumberGenerator = new Random();
+
+            // var randomNumber = randomNumberGenerator.Next(956);
+
+
+
+
+
+
+        }
     }
-  }
 }
