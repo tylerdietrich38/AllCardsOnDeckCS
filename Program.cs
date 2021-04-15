ab043dd91e7101822d3f8a716135826ed9677bf3
Tyler Dietrich <tylerdietrich38@gmail.com>
2021-04-15 16:44:51 -0400

    Revised Shuffle Mode

--- Program.cs
+++ Program.cs
@@ -17,6 +17,11 @@ namespace AllCardsOnDeckCS
 
             var deck = new List<string>();
 
+            // foreach (var suit in suits)
+            // {
+            //   foreach (var rank in ranks)
+            // }
+
 
             for (var index = 0; index < suits.Count; index++)
             {
@@ -33,25 +38,50 @@ namespace AllCardsOnDeckCS
 
             Console.WriteLine("------------------------------------------------");
 
+            // var numberOfCards = deck.Count;
+
+            // for (var rightIndex = numberOfCards - 1; rightIndex >= 0; rightIndex--)
+            // {
+            //     var leftIndex = new Random().Next(0, rightIndex);
+
+            //     var rightCard = deck[rightIndex];
+
+            //     deck[rightIndex] = deck[leftIndex];
+
+            //     deck[leftIndex] = rightCard;
+            // }
+
+            // foreach (var num in deck)
+            // {
+            //     Console.WriteLine($"{num}");
+
             var numberOfCards = deck.Count;
-
-            for (var rightIndex = numberOfCards - 1; rightIndex >= 0; rightIndex--)
+            // for rightIndex from numberOfCards - 1 down to 1 do:
+            for (var rightIndex = numberOfCards - 1; rightIndex >= 1; rightIndex--)
             {
-                var leftIndex = new Random().Next(0, rightIndex);
-
+                //   leftIndex = random integer that is greater than or equal to 0 and LESS than rightIndex. See the section "How do we get a random integer")
+                var randomNumberGenerator = new Random();
+                var leftIndex = randomNumberGenerator.Next(rightIndex);
+                //   Now swap the values at rightIndex and leftIndex by doing this:
+                //     leftCard = the value from deck[leftIndex]
+                var leftCard = deck[leftIndex];
+                //     rightCard = the value from deck[rightIndex]
                 var rightCard = deck[rightIndex];
-
-                deck[rightIndex] = deck[leftIndex];
-
+                //     deck[rightIndex] = leftCard
+                deck[rightIndex] = leftCard;
+                //     deck[leftIndex] = rightCard
                 deck[leftIndex] = rightCard;
             }
 
-            foreach (var num in deck)
-            {
-                Console.WriteLine($"{num}");
-            }
+            var firstCard = deck[0];
+            var secondCard = deck[1];
 
+            Console.WriteLine(firstCard);
+            Console.WriteLine(secondCard);
 
         }
+
+
     }
 }
+
